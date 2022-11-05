c68de59773b94627b812c3a607d9c3acb2cb1b01
Mateus Wesley <mateuswesley314@gmail.com>
2022-11-05 20:15:24 -0300

    versao-remover
    versão com botão de remover número

--- exercicios-javascript/Aula 15 - Variavéis Compostas/ex016/ex016.js
+++ exercicios-javascript/Aula 15 - Variavéis Compostas/ex016/ex016.js
@@ -1,24 +1,39 @@
 var lista = []
 var res = document.querySelector('div#res')
-var i = 1
+// select onde serão exibidos todos os números
+var print_lista = document.querySelector('select#print_lista')
+
+// acompanha a posição do número no selector
+var i = 0
+
+// objeto que vai associar cada número a sua 
+// posição no selector
+var obj_numeros = {}
 
 // função para adicionar valores a lista
 function adicionar(){
     // sempre que adicionar um número na lista reseta o resultado
     res.innerHTML = ''
 
+
     // número fornecido pelo usuário
     var txt_num = window.document.querySelector('input#numero')
     var num = Number(txt_num.value)
 
-    // select onde serão exibidos todos os números
-    var print_lista = document.querySelector('select#print_lista')
+    // adiciona o número ao object e aumenta i em 1
+    // para que o próximo número receba a posição seguinte.
+    obj_numeros[num] = i
+    i ++
+
+
+
     
     // verifica se o usuário forneceu algum número,
     //precisamos verificar diretamente a propriedade que possui o conteúdo (.value) não o elemento em si, txt_num.value é uma string, diferente de txt_num, que é object
     if(txt_num.value == ''){
         window.alert('Entre com um valor válido!')
     }else{
+
         // adciona o valor fornecido ao fim da lista,
         // criamos um elemento que vai armazenar a option que será adcionada ao select
         lista.push(num)
@@ -73,5 +88,28 @@ function finalizar(){
 
 }
     
+// função para remover elemento
+function remover(){
+    
+    var txt_num = window.document.querySelector('input#numero')
+    var num = Number(txt_num.value)
+    let indice_select = obj_numeros[num]
+    let indice_lista = lista.indexOf(num)
+    
+    if(indice_lista == -1){
+        window.alert('Esse número não existe na lista!')
+    }else{
+        i--
+        txt_num.value = ''
+        txt_num.focus()
+        res.innerHTML = ''
+        lista.splice(indice_lista, 1)
+        print_lista.remove(indice_select+1)
+    }
 
+    
+    
+
+
+}
 
