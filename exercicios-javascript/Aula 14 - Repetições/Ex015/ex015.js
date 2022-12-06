11fbcb17f7832913f7b8a71266bba70f59749d78
Mateus Wesley <mateuswesley314@gmail.com>
2022-12-06 20:13:49 -0300

    tabuada

--- exercicios-javascript/Aula 14 - Repetições/Ex015/ex015.js
+++ exercicios-javascript/Aula 14 - Repetições/Ex015/ex015.js
@@ -1,13 +1,15 @@
  function tabuada(){
     var txt_numero = document.querySelector('input#numero').value
     var tab = document.querySelector('select#tabuada')
+    var limite = document.querySelector('input#limite')
 
     if(txt_numero == ''){
         window.alert("Por favor digite um número!")
     }else{
-        var num_numero = Number(txt_numero)
+        let num_numero = Number(txt_numero)
+        let limite_tabuada = Number(limite.value)
         tab.innerHTML = ' '
-        for(var i=1;i<=10;i++){ 
+        for(var i=1;i<=limite_tabuada;i++){
             let item = document.createElement('option')
             item.text = `${num_numero}x${i}=${num_numero*i}`
             tab.appendChild(item)
